6e71bf956a0f51d807f19cea2e3fac8b2bfaea66
Levene <Levene@teacheraclevene.myfiosgateway.com>
2021-07-12 07:28:24 -0400

    Adding the sort lab

--- ds/sort1/SortDemo.java
+++ ds/sort1/SortDemo.java
@@ -1,6 +1,13 @@
 import java.io.*;
 import java.util.*;
-// Eric Liu, Dwayne Levene, Eduardo L
+
+/**
+ * Team 4
+ * Dwayne Levene
+ * Eduardo Leite
+ * Eric Liu tyl3452 tayou.liu@gmail.com
+ */
+
 /*
 Setup:
  1.Make a folder under your work repo named: ds
@@ -26,7 +33,7 @@ public class SortDemo{
     private ArrayList<Integer> data;  // to store the data
     private Random r; 
 
-    
+    //constructor loops 15 times to create a default array with 15 elements that are randome numbers from 1 to 19, 20 exclusive
     public SortDemo(){
 	data = new ArrayList<Integer>();
 	r = new Random();
@@ -36,6 +43,7 @@ public class SortDemo{
 	
     }
     
+    //Construct arraylist of target size with random numbers from 0 to 20 (exclusive)
     public SortDemo(int size){
 	data = new ArrayList<Integer>();
 	r = new Random();
@@ -45,6 +53,9 @@ public class SortDemo{
 	
     }
 
+// public int get(int index){
+//   return this.data.get(index);
+// }
     /*
       return the index of the ArrayList data from index start to the end
       Example, if the arraylist has:
@@ -53,78 +64,96 @@ public class SortDemo{
       6 which is the index with the smallest value from start to end
     */
     public int findSmallestIndex(int start){
-	int smallIndex = start;
+      int smallIndex = start;
+      //int smallestValue = data[start];
+      //for loop loop iterates from start  and sees if i is less that the size of the array list
+     // if so if the current position is less that the start the new smalles index is the current i, it keeps track of the smallest i
+      for(int i=start+1; i<data.size(); i++){
+        if(data.get(i) < data.get(smallIndex)) {
+          smallIndex = i;
+        }
+      }//end for
+      
+      // start a variable at the one after start
+      // your code here
+      
+      // loop from that variable to end and update smallIndex as needed
+      // your code here
 	
-	// start a variable at the one after start
-	// your code here
-	
-	// loop from that variable to end and update smallIndex as needed
-	// your code here
-	
-	return smallIndex;
+	    return smallIndex;
 
     }
 
 
     
     public void sort(){
-	int i;
-	for (i=0;i < data.size()-1; i++){
-	    // find the smallet index from i to end
-	    // your code here
+      int i;
+      int smallIndex;
+      int tempInt;
+      for (i=0;i < data.size()-1; i++){
+          // find the smallet index from i to end
+          // your code here
+          smallIndex = findSmallestIndex(i);
 
-	    // swap the item at that index and i
-	    // your code here
-	    
-	    
-	}
+          // swap the item at that index and i
+          // your code here
+          tempInt = data.get(smallIndex);
+          data.set(smallIndex, data.get(i));
+          data.set(i, tempInt);
+          
+      }
     }
 
 
 
     /* If you finish the lab early you can get started on this */
     public int linearSearch(int value){
-	// loop through the ArrayList data
-	// and if the value you're searchign for is in the ArrayList, return it.
-	// return -1 if it isn't there.
+      // loop through the ArrayList data
+      // and if the value you're searchign for is in the ArrayList, return it.
+      // return -1 if it isn't there.
+      
 	
-	
-	return 0; // replace this return
+	    return 0; // replace this return
     }
     
     /* If you finish the lab early you can get started on this */
     public int binarySearch(int value){
-	boolean replacethiswithrealexpression=false;
-	int lowerIndex = 0;
-	int upperIndex = data.size();
-	int middleIndex = data.size()/2;
-	
-	/* if upper crosses lower it's not there and the lop should exit the loop
-	   and if the item is at middle you should exit the loop
-    
-           you have to replace the "replacethiswithrealexpression" boolean 
-           with a correct expression based on lowerIndex and upperIndex
-	*/
-	while (replacethiswithrealexpression)
-	    {
-		// update lower and upper.
-		// remember if value is less than data.get(middleIndex) you want to search next time
-		// from lower to the middle and otherwise from the middle to the upper.
-		//
-		// then update middleIndex based on new lowerIndex and upperIndex.
-	    
-	    }
-	
-	/* replace this return to either return the value if it was found and -1
-	   if upperIndex and lowerIndex crossed
-	*/    
-	
-	return 0; // replace this return
+      boolean replacethiswithrealexpression=false;
+      int lowerIndex = 0;
+      int upperIndex = data.size();
+      int middleIndex = data.size()/2;
+      
+      /* if upper crosses lower it's not there and the lop should exit the loop
+        and if the item is at middle you should exit the loop
+        
+              you have to replace the "replacethiswithrealexpression" boolean 
+              with a correct expression based on lowerIndex and upperIndex
+      */
+      while (replacethiswithrealexpression)
+          {
+        // update lower and upper.
+        // remember if value is less than data.get(middleIndex) you want to search next time
+        // from lower to the middle and otherwise from the middle to the upper.
+        //
+        // then update middleIndex based on new lowerIndex and upperIndex.
+          
+          }
+      
+      /* replace this return to either return the value if it was found and -1
+        if upperIndex and lowerIndex crossed
+      */    
+      
+      return 0; // replace this return
     }
     
 	
     public String toString(){
-	return ""+data;
-    };
+	    return ""+data;
+    }
+
+    //added to be able to access in driver file
+    public int get(int index) {
+        return data.get(index);
+    }
 
 }
--- ds/sort1/SortDemoDriver.java
+++ ds/sort1/SortDemoDriver.java
@@ -1,5 +1,11 @@
 import java.io.*;
 import java.util.*;
+/**
+ * Team 4
+ * Dwayne Levene
+ * Eduardo Leite
+ * Eric Liu tyl3452 tayou.liu@gmail.com
+ */
 
 public class SortDemoDriver {
     public static void main(String[] args) {
@@ -11,19 +17,29 @@ public class SortDemoDriver {
 	System.out.println(sd2);
 		
 	/* part 2 test lines */
-	/*
-	  int i;
-	i = sd.findSmallestIndex();
-	System.out.println("sd["+"] = "+sd.get(i)+" : " + sd);
-	i = sd2.findSmallestIndex();
-	System.out.println("sd2["+"] = "+sd2.get(i)+" : " + sd2);
+	
+  int i;
+	// i = sd.findSmallestIndex(0);
+	// System.out.println("sd["+"] = "+sd.get(i)+" : " + sd);
+	// i = sd2.findSmallestIndex(0);
+	// System.out.println("sd2["+"] = "+sd2.get(i)+" : " + sd2);
+  i = sd.findSmallestIndex(0);
+  System.out.println("sd["+ i + "] = " + sd.get(i) + " : " + sd);
+	i = sd2.findSmallestIndex(0);
+	System.out.println("sd2["+ i + "] = " + sd2.get(i) + " : " + sd2);
+  /*
 	*/
 	
 	/* part 3 test lines */
-	/*
-	  System.out.println(sd);
+	
+	  System.out.println("sd Pre-sorted:" + sd);
 	  sd.sort();
-	  System.out.println(sd);
+	  System.out.println("sd After sorted: " + sd);
+
+    System.out.println("sd2 Pre-sorted:" + sd2);
+	  sd2.sort();
+	  System.out.println("sd2 After sorted: " + sd2);
+    /*
 	*/
     }
 }
