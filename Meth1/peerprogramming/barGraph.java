c07baf0e66d1b9113b1a7c196b0b7e539f188fae
Levene <Levene@teacheraclevene.myfiosgateway.com>
2021-07-15 20:35:50 -0400

    Checking to make sure Linked list works

---
+++ Meth1/peerprogramming/barGraph.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class barGraph {
+  //Dwayne and Marina
+    public static void barGraphy(int [] nums){
+    for (int i=0; i < nums.length; i++){ //from 0 to 4
+    System.out.print ("i="+i);
+    }  
+      for (int j=0; j<nums[i]; j++);{  //
+        System.out.print (nums[i]);
+        System.out.print ("=");
+      }
+    System.out.println();
+    }
+    
+
+
+  
+
+  public static void main(String[] args) {
+    int [] x =  {0,1,2,3,4};
+    //barGraphy(x);
+
+  }
+
+}
