235b71a6cd34375ec2ef9dd42ae8c9e29bdbe547
Dwayneglevene <dwayneglevene@yahoo.com>
2021-07-26 10:36:30 -0400

    updated binary tree traverse

--- ds/trees/BSTree.java
+++ ds/trees/BSTree.java
@@ -1,52 +1,95 @@
 import java.io.*;
 import java.util.*;
 
+public class BSTree {
+    private TreeNode root;
 
-public class BSTree{
-	private TreeNode root;
+    public BSTree(){
+	root = null;
+    }
 
-	public BSTree(){
-		root = null;
-	}
-
-	public int search(int key){
-		TreeNode newNode = new TreeNode(key);
-
-		//if tree is emppty manually insert new node as root
-
-		if(root == null){
-			root = newNode;
+	public void traverse(TreeNode current){
+		if(current == null)
 			return;
-		}
-		TreeNode front = root;
-		TreeNode trailer;
 
-		while(front != null){
-			int frontValue = front.getData();
-			if (frontValue ==key){
-				//if key is here it means the key is 
-				//already in the node in someway
-				//then just return
-				return frontValue;
-			}else if (frontValue < key){
-				trailer = front;
-				front = front.getRight();
-			}else {
-				trailer = front;
-				front = front.getLeft();
-			}
-		}
-		if(key > front.getVale()){
-			//insert on the right
-			front.setRight(newNode);
-		}else{
-			//inser on left
-			front.setLeft(newNode);
-		}
+		//process the current node
+
+		System.out.print(current.getData() + ",");
+
+		//recurseively print out the left subtree
+
+		//recursively print out the right subtree
+		traverse(current.getRight());
 	}
 
-	public void seed(){
-		TreeNode t;
+	public void traverse(){
+		traverse(root);
+	}
+
+
+    public void insert(int key){
+
+	TreeNode newNode = new TreeNode(key);
+
+	// if the tree is empty
+	// manually insert the new node as the root
+	if (root==null){
+	    root = newNode;
+	    return;
+	}
+	
+	TreeNode front = root;
+	TreeNode trailer = root;
+	
+	while (front != null){
+	    int frontValue = front.getData();
+	    if (frontValue == key){
+		// if we're here, it means the key is
+		// already in the tree so we can
+		// update this node in some way
+		// and then return
+		return;
+	    } else if (frontValue < key){
+		trailer = front;
+		front = front.getRight();
+	    } else {
+		trailer = front;
+		front = front.getLeft();
+	    }
+	}
+	if (key > trailer.getData()){
+	    // insert on the right
+	    trailer.setRight(newNode);
+	} else {
+	    // insert on left
+	    trailer.setLeft(newNode);
+
+	}
+	
+	
+
+    }
+
+
+    public int search(int key){
+	TreeNode current = root;
+
+	while (current != null){
+	    int currentValue = current.getData();
+	    if (currentValue == key){
+		return currentValue;
+	    } else if (currentValue < key){
+		current = current.getRight();
+	    } else {
+		current = current.getLeft();
+	    }
+	}
+	throw new NullPointerException();
+
+    }
+    
+    public void seed(){
+	TreeNode t;
 
 	t = new TreeNode(10);
 	root = t;
@@ -62,5 +105,8 @@ public class BSTree{
 
 	t = new TreeNode(22);
 	root.getRight().setRight(t);
+	
 	}
+    
+    
 }
--- ds/trees/Driver.java
+++ ds/trees/Driver.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 
@@ -8,18 +7,31 @@ public class Driver {
 		BSTree t = new BSTree();
 
 		t.seed();
-	
 		int value;
-		value = t.search(10);
-		System.out.println(value);
-		value = t.search(15);
-		System.out.println(value);
-		try {
-			value = t.search(17);
-			System.out.println(value);
-		} catch (NullPointerException e) {
-			System.out.println("17 not in tree");
-		}
-		
+
+		// value = t.search(10);
+		// System.out.println(value);
+
+		// value = t.search(15);
+		// System.out.println(value);
+
+		// try {
+		// 	value = t.search(17);
+		// 	System.out.println(value);
+		// } catch (NullPointerException e) {
+		// 	System.out.println("17 not in tree");
+		// }
+
+		t.insert(10);
+		t.insert(20);
+		t.insert(5);
+		t.insert(7);
+		t.insert(8);
+		t.insert(3);
+
+		t.traverse();
+
+
+
 	}
 }
