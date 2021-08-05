fa77f3dd373dc998a0c88b542ee38fc049686bf8
Dwayneglevene <dwayneglevene@yahoo.com>
2021-08-05 15:44:46 -0400

    working quicksort algorithm will add comments next

---
+++ ds/project2_QuickSort/Driver.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+/**
+ * Team 4
+ * Dwayne Levene
+ * Eduardo Leite
+ * Eric Liu tyl3452 tayou.liu@gmail.com
+ */
+
+public class Driver {
+    public static void main(String[] args) {
+
+	    Quicksort(6,5,4,3,7,9);
+
+    }
+}
